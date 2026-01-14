c5e22b36446c650346bae7bc31f50b8c72cd97c4
David Schröder <post@schroederdavid.de>
2026-01-14 07:43:03 +0100

    Add copyright texts to clocking files

--- src/rtl/rv_clk/rvlab_clk_reconfig.sv
+++ src/rtl/rv_clk/rvlab_clk_reconfig.sv
@@ -1,4 +1,6 @@
-/* Copyright David Schröder 2025. */
+// SPDX-License-Identifier: SHL-2.1
+// SPDX-FileCopyrightText: 2025 RVLab Contributors
+
 /* TL-UL <-> TL-UL Variable Clocking manager */
 /* Allows the getting and setting of the current system clock divider */
 /* Implements a CDC to prevent spurious resets on reconfiguration */
