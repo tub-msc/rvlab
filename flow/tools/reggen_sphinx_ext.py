b69784a759764a64d6b757c3cac367dbaefd337c
Tobias Kaiser <kaiser@tu-berlin.de>
2026-07-24 19:34:56 +0200

    add regdef format documentation (through reggen.gen_selfdoc)

--- flow/tools/reggen_sphinx_ext.py
+++ flow/tools/reggen_sphinx_ext.py
@@ -5,7 +5,11 @@ from docutils import nodes
 from docutils.parsers.rst import Directive, directives
 
 from .reggen_wrapper import run_reggen
+from .reggen import gen_selfdoc
 from pathlib import Path
+import io
+import mistletoe
+import re
 import tempfile
 
 class ReggenDirective(Directive):
@@ -28,8 +32,29 @@ class ReggenDirective(Directive):
 
         return [node]
 
+class ReggenSelfdocDirective(Directive):
+    required_arguments = 0
+    optional_arguments = 0
+    has_content = False
+
+    def run(self):
+        buf = io.StringIO()
+        gen_selfdoc.document(buf)
+
+        html = mistletoe.markdown(buf.getvalue())
+
+        # Mistletoe emits bare tags; add the classes that Sphinx themes style.
+        html = html.replace('<table>', '<table class="docutils align-default">')
+        html = re.sub(r'<pre><code[^>]*>', '<div class="highlight"><pre>', html)
+        html = html.replace('</code></pre>', '</pre></div>')
+
+        node = nodes.raw('', html, format='html')
+
+        return [node]
+
 def setup(app):
     app.add_directive("reggen", ReggenDirective)
+    app.add_directive("reggen-selfdoc", ReggenSelfdocDirective)
     app.add_css_file('css/reg_html.css') 
 
     return {
